a4c9adaabddf86f4f1b94077e96bd5e435b69fae
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2018-04-09 13:45:34 +0200

    Scaffold the Group Detail component and files

---
+++ src/app/features/groups/group-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-group-detail',
+  templateUrl: './group-detail.component.html',
+  styles: []
+})
+export class GroupDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/features/groups/group-module-routing.module.ts
+++ src/app/features/groups/group-module-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CreateGroupComponent } from './create-group.component';
+import { GroupDetailComponent } from './group-detail.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  declarations: [CreateGroupComponent]
+  declarations: [CreateGroupComponent, GroupDetailComponent]
 })
 export class GroupModuleRoutingModule { }
