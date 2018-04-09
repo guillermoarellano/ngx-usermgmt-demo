6ea1beb4437a6d01a7c3f60a858eba2c81e6875d
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2018-04-09 14:40:47 +0200

    Defining the routes to components for the Users feature module

--- src/app/features/users/user-routing.module.ts
+++ src/app/features/users/user-routing.module.ts
@@ -5,11 +5,22 @@ import { UserDetailComponent } from './user-detail.component';
 import { UserMembershipComponent } from './user-membership.component';
 import { UsersListComponent } from './users-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UsersListComponent },
+  { path: 'new', component: CreateUserComponent },
+  { path: ':userId/membership', component: UserMembershipComponent },
+  { path: ':userId', component: UserDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
-  declarations: [CreateUserComponent, UserDetailComponent, UserMembershipComponent, UsersListComponent]
+  exports: [RouterModule]
 })
 export class UserRoutingModule { }
+
+export const routedUserComponents = [
+  UsersListComponent,
+  UserDetailComponent,
+  CreateUserComponent,
+  UserMembershipComponent
+];
--- src/app/features/users/user.module.ts
+++ src/app/features/users/user.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { UserRoutingModule } from './user-routing.module';
+import { routedUserComponents } from './user-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     UserRoutingModule
   ],
-  declarations: []
+  declarations: [
+    routedUserComponents
+  ]
 })
 export class UserModule { }
