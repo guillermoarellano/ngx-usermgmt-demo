b2277361233484615d999c6ba05184a087afc392
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2018-04-09 14:06:17 +0200

    Scaffold the User Detail component and files

--- src/app/features/users/user-routing.module.ts
+++ src/app/features/users/user-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CreateUserComponent } from './create-user.component';
+import { UserDetailComponent } from './user-detail.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  declarations: [CreateUserComponent]
+  declarations: [CreateUserComponent, UserDetailComponent]
 })
 export class UserRoutingModule { }
