8268b7f9e44f26d464a7f118d23cde46b4c255c1
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2018-04-09 14:08:31 +0200

    Scaffold the Users List component and files

--- src/app/features/users/user-routing.module.ts
+++ src/app/features/users/user-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { CreateUserComponent } from './create-user.component';
 import { UserDetailComponent } from './user-detail.component';
 import { UserMembershipComponent } from './user-membership.component';
+import { UsersListComponent } from './users-list.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  declarations: [CreateUserComponent, UserDetailComponent, UserMembershipComponent]
+  declarations: [CreateUserComponent, UserDetailComponent, UserMembershipComponent, UsersListComponent]
 })
 export class UserRoutingModule { }
