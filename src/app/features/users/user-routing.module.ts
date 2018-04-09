124d89f0e9c0440703f0b4a2626c7a2cfff4b0d0
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2018-04-09 14:04:59 +0200

    Scaffold the Create Group component and files

--- src/app/features/users/user-routing.module.ts
+++ src/app/features/users/user-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateUserComponent } from './create-user.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [CreateUserComponent]
 })
 export class UserRoutingModule { }
