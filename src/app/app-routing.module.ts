d6542f0da460c0c7e0a0a246815ed0562892c294
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2018-04-09 14:20:35 +0200

    Created an app wide routing module that will load the necessary routes for the navigation

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { WelcomeComponent } from './home/welcome.component';
+
+const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  {
+    path: 'users', 
+    loadChildren: 'app/features/users/user.module#UserModule'
+  },
+  {
+    path: 'groups', 
+    loadChildren: 'app/features/groups/group.module#GroupModule'
+  },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+//   { path: '**', component: PageNotFoundComponent } TODO
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
+
+export const routedComponents = [
+    WelcomeComponent
+];
