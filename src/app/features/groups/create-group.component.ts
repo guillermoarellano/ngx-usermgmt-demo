04762b75aac185b61a170394143d4a1f1c0160d3
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2018-04-09 15:01:00 +0200

    Implemented the Create Group component

--- src/app/features/groups/create-group.component.ts
+++ src/app/features/groups/create-group.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { GroupService } from './services/group.service';
+import { Group } from './models/group.model';
 
 @Component({
   selector: 'app-create-group',
-  templateUrl: './create-group.component.html',
-  styles: []
+  templateUrl: './create-group.component.html'
 })
 export class CreateGroupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private groupService: GroupService,
+              private router: Router) { }
 
   ngOnInit() {
   }
 
+  cancel() {
+    this.router.navigate(['groups']);
+  }
+
+  onSubmit(formValues) {
+    let newGroup: Group = {
+      id: 0,
+      groupName: formValues.groupName,
+      groupMembers: []
+    }
+    
+    this.groupService.createGroup(newGroup).subscribe(
+      (data) => console.log(data),
+      (err) => console.log(err)
+    );
+
+    this.router.navigate(['groups']);
+  }
 }
