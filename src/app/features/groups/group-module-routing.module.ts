7c2de5812e368b674d20d795cfe2646a8e4d6d7a
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2018-04-09 13:48:15 +0200

    Scaffold the Groups List component and files

--- src/app/features/groups/group-module-routing.module.ts
+++ src/app/features/groups/group-module-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { CreateGroupComponent } from './create-group.component';
 import { GroupDetailComponent } from './group-detail.component';
 import { GroupMembersComponent } from './group-members.component';
+import { GroupsListComponent } from './groups-list.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  declarations: [CreateGroupComponent, GroupDetailComponent, GroupMembersComponent]
+  declarations: [CreateGroupComponent, GroupDetailComponent, GroupMembersComponent, GroupsListComponent]
 })
 export class GroupModuleRoutingModule { }
