de50049887113502ec589516cade62092dcf8481
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2018-04-09 14:07:10 +0200

    Scaffold the User Membership component and files

---
+++ src/app/features/users/user-membership.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-membership',
+  templateUrl: './user-membership.component.html',
+  styles: []
+})
+export class UserMembershipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/features/users/user-routing.module.ts
+++ src/app/features/users/user-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CreateUserComponent } from './create-user.component';
 import { UserDetailComponent } from './user-detail.component';
+import { UserMembershipComponent } from './user-membership.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  declarations: [CreateUserComponent, UserDetailComponent]
+  declarations: [CreateUserComponent, UserDetailComponent, UserMembershipComponent]
 })
 export class UserRoutingModule { }
