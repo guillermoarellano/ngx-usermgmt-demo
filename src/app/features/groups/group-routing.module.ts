18d431f3eaf15e6ec2f6e2c8375303a4d55a6163
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2018-04-09 14:37:38 +0200

    Defining the routes to components for the Groups feature module

--- src/app/features/groups/group-routing.module.ts
+++ src/app/features/groups/group-routing.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { CreateGroupComponent } from './create-group.component';
 import { GroupDetailComponent } from './group-detail.component';
 import { GroupMembersComponent } from './group-members.component';
 import { GroupsListComponent } from './groups-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: GroupsListComponent },
+  { path: 'new', component: CreateGroupComponent },
+  { path: ':groupId/members', component: GroupMembersComponent },
+  { path: ':groupId', component: GroupDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
-  declarations: [CreateGroupComponent, GroupDetailComponent, GroupMembersComponent, GroupsListComponent]
+  exports: [RouterModule]
 })
 export class GroupRoutingModule { }
+
+export const routedGroupComponents = [
+  GroupsListComponent,
+  GroupDetailComponent,
+  CreateGroupComponent,
+  GroupMembersComponent
+];
--- src/app/features/groups/group.module.ts
+++ src/app/features/groups/group.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { GroupRoutingModule } from './group-routing.module';
+import { routedGroupComponents } from './group-routing.module';
+
 
 @NgModule({
   imports: [
+    FormsModule,
     CommonModule,
     GroupRoutingModule
   ],
-  declarations: []
+  declarations: [
+    routedGroupComponents
+  ]
 })
 export class GroupModule { }
