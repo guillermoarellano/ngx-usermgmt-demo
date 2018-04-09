881ad70bc4673621f47251f667d76959a45fc8a0
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2018-04-09 13:46:55 +0200

    Scaffold the Group Members component and files

---
+++ src/app/features/groups/group-members.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-group-members',
+  templateUrl: './group-members.component.html',
+  styles: []
+})
+export class GroupMembersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/features/groups/group-module-routing.module.ts
+++ src/app/features/groups/group-module-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CreateGroupComponent } from './create-group.component';
 import { GroupDetailComponent } from './group-detail.component';
+import { GroupMembersComponent } from './group-members.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  declarations: [CreateGroupComponent, GroupDetailComponent]
+  declarations: [CreateGroupComponent, GroupDetailComponent, GroupMembersComponent]
 })
 export class GroupModuleRoutingModule { }
