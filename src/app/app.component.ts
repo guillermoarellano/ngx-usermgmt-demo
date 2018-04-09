b34bb299303b88d0ef3c2b91909f33a4e2ca5ac1
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2018-04-09 13:27:54 +0200

    Defined the main modules that will need to be imported into App. App component shell built.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'User Management App';
+  loading: boolean = true;
+  isCollapsed: boolean = true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CollapseModule } from 'ngx-bootstrap';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+// import { InMemoryDataService } from './in-memory-data.service';
 
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { routedComponents } from './app-routing.module';
+// import { UserService } from './features/users/services/user.service';
+// import { GroupService } from './features/groups/services/group.service';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    CollapseModule,
+    // AppRoutingModule,
+    // HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    // routedComponents,
+  ],
+  providers: [
+    // UserService,
+    // GroupService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
